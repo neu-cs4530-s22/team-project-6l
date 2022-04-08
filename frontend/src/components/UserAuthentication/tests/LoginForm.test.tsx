112aaf07e82b6eca256b2a3ef090211a48e2932d
Ngoc (Victoria) Le <58549806+khanhvy1703@users.noreply.github.com>
2022-04-07 20:59:04 -0400

    LoginForm tests

--- frontend/src/components/UserAuthentication/tests/LoginForm.test.tsx
+++ frontend/src/components/UserAuthentication/tests/LoginForm.test.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render, fireEvent } from '@testing-library/react';
-import { renderHook } from "@testing-library/react-hooks";
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import LoginForm from '../LoginForm';
 
 jest.mock('../../../firebase/firebase-config', () => ({
@@ -21,17 +20,42 @@ jest.mock('../../../firebase/firebase-config', () => ({
 }));
 
 describe('LoginForm', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should render all the elements properly without Alert box', () => {
-    const { getByTestId } = render(<LoginForm />);
+    const { getByTestId, queryByTestId } = render(<LoginForm />);
+
     const email = getByTestId('login-email');
     const password = getByTestId('login-password');
     const forgotPassBtn = getByTestId('forgot-password-btn');
     const signInBtn = getByTestId('sign-in-btn');
+    const alertErr = queryByTestId('alert-error');
+
     expect(email).toBeInTheDocument();
     expect(password).toBeInTheDocument();
     expect(forgotPassBtn).toBeInTheDocument();
     expect(signInBtn).toBeInTheDocument();
+    expect(alertErr).toBeNull(); // Alert UI comp should be rendered. 
   });
+  it('should render Alert error with \'Enter your email\' error mess', async () => {
+    render(<LoginForm />);
+
+    // check to make sure the 'Enter your email' text is yet rendered
+    expect(screen.queryByText('Enter your email')).toBeNull()
+    const signInBtn = screen.getByTestId('sign-in-btn');
+    fireEvent.click(signInBtn);
+
+    // this time 'Enter your email' text is rendered
+    await waitFor(() => {
+      expect(screen.queryByText('Enter your email')).toBeDefined();
+    })
+  });
+  it('should render Alert error with \'Enter your password\' error mess', () => {
+    render(<LoginForm />);
+    
+  })
 })
 
 
